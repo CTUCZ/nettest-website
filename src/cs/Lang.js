7891912675ec2876cc4f45d3763c1a12553b4bfb
David Furmánek <furmanekd@gmail.com>
2021-09-08 13:22:46 +0200

    CertMeasurement - provider_name changed to provier_name_cm

--- src/cs/Lang.js
+++ src/cs/Lang.js
@@ -312,7 +312,7 @@ Lang.setStrings({
     'enter_provider_name': 'Zadejte název poskytovatele služby',
     'enter_provider_service_name': 'Zadejte název služby',
     'enter_provider_contract_number': 'Zadejte číslo smlouvy',
-    'download_results_as_pdf': 'Download results as PDF',
+    'download_results_as_pdf': 'Stáhnout výsledky ve formátu PDF',
     'alert_close_tab': 'Do you want to cancel the measurement?',
     'help': 'Nápověda'
 });
