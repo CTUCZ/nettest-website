40256e063338a2c8ef901ea6d557316e51f0596d
simonikj <simonikj@ctu.cz>
2021-09-20 16:00:38 +0200

    URL & info update

--- lang/cs.js
+++ lang/cs.js
@@ -268,6 +268,7 @@ exports.strings = {
     "home": "Domů",
     "link_to_certified": "<a href='/cs/CertMeasurement'>Pro účely reklamace služby využijte certifikovaného měření.</a>",
     "loop_tab_warning": "Upozorňujeme uživatele, že pro správný průběh opakovaného měření, je nutné mít aktivní záložku s měřením v prohlížeči, jinak se nemusí spustit následný test.",
+    "cert_tab_warning": "Upozorňujeme uživatele, že pro správný průběh certifikovaného měření, je nutné mít aktivní záložku s měřením v prohlížeči, jinak se nemusí spustit následný test.",
 
     //common words
     "top" : "Top",
